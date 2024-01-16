9a85d5377805cb623d29ebcc5988040275524bb8
Suryapratap Singh Suryavanshi <suryprtaps@gmail>
2024-01-16 23:57:51 +0530

    Added fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+/*
+    Author: Suryapratap Singh Suryavanshi
+    Date: 01/16/2024
+    Version: 1.0.0
+*/
+
+#include<stdio.h>
+
+// Method: return n'th fibonacci number, recursive approach
+int fib(int n) {
+    if(n == 0 || n == 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int res = fib(n);
+    printf("%d'th fibonacci number is: %d", n, res);
+}
